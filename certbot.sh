076f45ee0107484faca2c3a16d12d7d16887bbf8
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-08-04 23:36:53 -0500

    added new subdomains

--- certbot.sh
+++ certbot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-certbot certonly --expand --webroot --webroot-path /var/www/html/letsencrypt -d storyoftheseason.co
+certbot certonly --expand --webroot --webroot-path /var/www/html/letsencrypt -d storyoftheseason.co -d mail.storyoftheseason.co -d email.storyoftheseason.co
