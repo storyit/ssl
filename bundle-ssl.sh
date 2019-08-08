1ed2775bd699a00812dbfcb166da9f9ceb9413c9
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-08-08 12:03:30 -0400

    migrating to .com

--- bundle-ssl.sh
+++ bundle-ssl.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
 
-cat /etc/letsencrypt/live/storyoftheseason.co/fullchain.pem /etc/letsencrypt/live/storyoftheseason.co/privkey.pem > /etc/ssl/haproxy/storyoftheseason.co.pem
+cat /etc/letsencrypt/live/storyoftheseason.com/fullchain.pem /etc/letsencrypt/live/storyoftheseason.com/privkey.pem > /etc/ssl/haproxy/storyoftheseason.com.pem
--- certbot.sh
+++ certbot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-certbot certonly --expand --webroot --webroot-path /var/www/html/letsencrypt -d storyoftheseason.co -d mail.storyoftheseason.co -d email.storyoftheseason.co
+certbot certonly --expand --webroot --webroot-path /var/www/html/letsencrypt -d storyoftheseason.com -d mail.storyoftheseason.com -d email.storyoftheseason.com
