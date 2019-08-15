bdca7c86c510d39b11868ad790656d534dc6c1e4
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-08-15 02:46:15 -0400

    still need ssl cert for .co even if it just redirects to .com

--- bundle-ssl.sh
+++ bundle-ssl.sh
@@ -2,3 +2,4 @@
 #!/bin/bash
 
 cat /etc/letsencrypt/live/storyoftheseason.com/fullchain.pem /etc/letsencrypt/live/storyoftheseason.com/privkey.pem > /etc/ssl/haproxy/storyoftheseason.com.pem
+cat /etc/letsencrypt/live/storyoftheseason.co/fullchain.pem /etc/letsencrypt/live/storyoftheseason.co/privkey.pem > /etc/ssl/haproxy/storyoftheseason.co.pem
--- certbot.sh
+++ certbot.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 certbot certonly --expand --webroot --webroot-path /var/www/html/letsencrypt -d storyoftheseason.com -d mail.storyoftheseason.com -d email.storyoftheseason.com
+certbot certonly --expand --webroot --webroot-path /var/www/html/letsencrypt -d storyoftheseason.co -d mail.storyoftheseason.co -d email.storyoftheseason.co
